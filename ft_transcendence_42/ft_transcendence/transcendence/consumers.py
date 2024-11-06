8d6ece83780c5687d1ed89227844b17af3c4debb
Mikhail Dogadin <mdogadin@c1r5s1.42prague.com>
2024-11-06 17:26:52 +0100

    pong doenst work

--- ft_transcendence_42/ft_transcendence/transcendence/consumers.py
+++ ft_transcendence_42/ft_transcendence/transcendence/consumers.py
@@ -1,5 +1,6 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 class Calculator(WebsocketConsumer):
     def connect(self):
@@ -18,3 +19,42 @@ class Calculator(WebsocketConsumer):
         self.send(text_data=json.dumps({
             'result': result
         }))
+
+
+class GameConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = f"game_{self.room_name}"
+
+        # Присоединяем игрока к комнате
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Отключаем игрока от комнаты
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        
+        # Отправляем данные всем игрокам в комнате
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'game_message',
+                'message': data
+            }
+        )
+
+    async def game_message(self, event):
+        message = event['message']
+
+        # Отправляем сообщение игроку
+        await self.send(text_data=json.dumps(message))
--- ft_transcendence_42/ft_transcendence/transcendence/routing.py
+++ ft_transcendence_42/ft_transcendence/transcendence/routing.py
@@ -4,4 +4,5 @@ from . import consumers
 
 websocket_urlpatterns = [
     re_path(r'ws/livec/$', consumers.Calculator.as_asgi()),
-]
+    re_path(r'ws/game/(?P<room_name>\w+)/$', consumers.GameConsumer.as_asgi()),
+ ]
