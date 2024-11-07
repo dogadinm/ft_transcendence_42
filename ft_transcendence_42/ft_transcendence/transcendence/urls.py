ecd2e5ca92023a9c7e7912513b65d24452212205
Mikhail Dogadin <mdogadin@c2r7s3.42prague.com>
2024-11-07 17:22:06 +0100

    added navigate bar

--- ft_transcendence_42/ft_transcendence/transcendence/urls.py
+++ ft_transcendence_42/ft_transcendence/transcendence/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
-from django.urls import path, re_path, register_converter
+from django.urls import path, re_path
 
 from . import views
 from . import converters
 
 
 urlpatterns = [
-    path('', views.index),
-    re_path(r'calculator/', views.calculator, name="index"),
-    path('cats/<int:cat_id>/', views.categories),
-    path('cats/<slug:cat_slug>/', views.categories_by_slug),
+    path('', views.index, name='index'),
+    path("login/", views.login, name="login"),
+    path("register/", views.register, name="register"),
+    re_path(r'pong/', views.pong, name="pong"),
+    re_path(r'calculator/', views.calculator, name="calculator"),
 ]
--- ft_transcendence_42/ft_transcendence/transcendence/views.py
+++ ft_transcendence_42/ft_transcendence/transcendence/views.py
@@ -1,11 +1,34 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.urls import reverse
+from django.core.paginator import Paginator
+import json
+from django.http import JsonResponse
 
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'pong_app/index.html')
+    # posts = Post.objects.all().order_by('id').reverse()
+
+    # Pagination
+    # p = Paginator(posts, 10)
+    # numb_page = request.GET.get("page")
+    # post_page = p.get_page(numb_page)
+
+
+    return render(request, "pong_app/index.html")
+
+
+def login(request):
+    return render(request, 'pong_app/login.html')
+def register(request):
+    return render(request, 'pong_app/register.html')
+
+def pong(request):
+    return render(request, 'pong_app/pong.html')
 
 def categories(request, cat_id):
     return HttpResponse(f"<h1>Categories<h1><p>id: {cat_id}<p>")
