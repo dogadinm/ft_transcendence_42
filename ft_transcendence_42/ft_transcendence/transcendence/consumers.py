959bca985430a20734b54dca66bc17f19fc536e4
Mikhail Dogadin <mdogadin@c3r6s1.42prague.com>
2024-12-02 15:47:20 +0100

    chat work

--- ft_transcendence_42/ft_transcendence/transcendence/consumers.py
+++ ft_transcendence_42/ft_transcendence/transcendence/consumers.py
@@ -1,10 +1,11 @@
 import json
 import asyncio
-from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer, WebsocketConsumer
 from channels.db import database_sync_to_async
 from .models import Room
 from django.contrib.auth import get_user_model
 from .game import room_manager
+from asgiref.sync import async_to_sync
 
 class GameConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -116,3 +117,41 @@ class GameConsumer(AsyncWebsocketConsumer):
             self.room_game.players['right'] = self.username
             return 'right'
         return 'spectator'
+
+
+
+class ChatConsumer(WebsocketConsumer):
+    def connect(self):
+        self.room_group_name = 'test'
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        self.accept()
+        self.send(text_data=json.dumps({
+            'type':'connection_established',
+            'message':'You are now connected'
+        }))
+
+    #def disconnect(self, close_code):
+
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        print(message)
+        
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type':'chat_message',
+                'message':message
+            }
+        )
+    def chat_message(self, event):
+        message = event['message']
+
+        self.send(text_data=json.dumps({
+            'type':'chat',
+            'message':message
+        }))
--- ft_transcendence_42/ft_transcendence/transcendence/routing.py
+++ ft_transcendence_42/ft_transcendence/transcendence/routing.py
@@ -4,4 +4,6 @@ from . import consumers
 
 websocket_urlpatterns = [
     re_path(r'ws/game/(?P<room_name>\w+)/$', consumers.GameConsumer.as_asgi()),
+    re_path(r'ws/socet-server/', consumers.ChatConsumer.as_asgi()),
+
  ]
